06dd572037ca3334deb372cf46b87dddc336de6f
Ga-vin <gavin_8724@163.com>
2016-08-29 19:17:25 -0400

    Finish clear list and get item by index. 1. Finish clear all nodes of list; 2. Finish getting item by specific index in the list.

--- polynomial.c
+++ polynomial.c
@@ -6,7 +6,14 @@
 
 #include "polynomial.h"
 
-#define    GET_NEXT_NODE(list)    (list->next)
+/* Name     : _list_get_next                                                 */
+/* Function : Get next node of list                                          */
+/* Input    : p_node -- specific node of list                                */
+/* Output   : Next node of list                                              */
+static PPoly _list_get_next(const PPoly p_node)
+{
+    return (p_node->next);
+}
 
 /* Name     : init_list                                                      */
 /* Function : Initialize list node with malloc                               */
@@ -40,14 +47,121 @@ STATE destroy_list(PPoly p_header)
 #endif        
     }
 
-    p_node = GET_NEXT_NODE(p_header);
+    p_node = _list_get_next(p_header);
     while ( p_temp) {
-        p_temp = GET_NEXT_NODE(p_node);
+        p_temp = _list_get_next(p_node);
         free(p_node);
         p_node = p_temp;
     }
     free(p_header);
     p_header = LIST_NULL;
+
+    return (TRUE);
+}
+
+/* Name     : clear_list                                                     */
+/* Function : Clear list node one by one for all the list                    */
+/* Input    : p_header    ---   header of list pointer                       */
+/* Output   : Cleared successfully, TRUE will be returned, or else FALSE   */
+STATE clear_list(PPoly p_header)
+{
+    PPoly p_node = LIST_NULL;
+    PPoly p_temp = LIST_NULL;
+    
+    if ( !p_header) {
+#ifdef __DEBUG_PRINTF
+        fprintf(stderr, "[x] Header pointer is NULL. \n");
+#endif /* __DEBUG_PRINTF */
+
+        return (FALSE);
+    }
+
+    p_node = _list_get_next(p_header);
+    while ( p_node) {
+        p_temp = _list_get_next(p_node);
+        free(p_node);
+
+        p_node = _list_get_next(p_node);
+    }
+    p_header->next = LIST_NULL;
+
+    return (TRUE);
+}
+
+/* Name     : get_length_list                                                */
+/* Function : Get how many nodes in the link list                            */
+/* Input    : p_header    ---   header of list pointer                       */
+/* Output   : How many nodes in the list                                     */
+INT32 get_length_list(const PPoly p_header)
+{
+    PPoly p_node = LIST_NULL;
+    INT32 cnt    = 0;
+    
+    if ( !p_header) {
+#ifdef __DEBUG_PRINTF
+        fprintf(stderr, "[x] Header pointer is NULL. \n");
+#endif /* __DEBUG_PRINTF */
+
+        return (-1);
+    }
+
+    p_node = _list_get_next(p_header);
+    while ( p_node) {
+        ++cnt;
+
+        p_node = _list_get_next(p_node);
+    }
+
+    return (cnt);
+}
+
+/* Name     : get_item_by_index_list                                         */
+/* Function : Get item of node by specific index                             */
+/* Input    : p_header    ---   header of list pointer
+              index       ---   specific index
+              p_term      ---   to store node has found                      */
+/* Output   : If found, TRUE will be returned, or else FALSE                 */
+STATE get_item_by_index_list(const PPoly p_header,
+                             UINT32      index,
+                             TermType   *p_term)
+{
+    PPoly  p_node = LIST_NULL;
+    UINT32 cnt    = 0;
+    
+    if ( !p_header) {
+#ifdef __DEBUG_PRINTF
+        fprintf(stderr, "[x] Header pointer is NULL. \n");
+#endif /* __DEBUG_PRINTF */
+
+        memset(p_term, 0, sizeof(TermType));
+        return (FALSE);
+    }
+
+    if ( !p_term) {
+#ifdef __DEBUG_PRINTF
+        fprintf(stderr, "[x] Storage space for term is NULL. \n");
+#endif /* __DEBUG_PRINTF */
+
+        memset(p_term, 0, sizeof(TermType));
+        return (FALSE);
+    }
+
+    if (  index > get_length_list(p_header)) {
+#ifdef __DEBUG_PRINTF
+        fprintf(stderr, "[x] Index has been exceeded. \n");
+#endif /* __DEBUG_PRINTF */
+
+        memset(p_term, 0, sizeof(TermType));
+        return (FALSE);
+    }
+
+    p_node = _list_get_next(p_header);
+    while ( p_node && (cnt < index)) {
+        ++cnt;
+        p_node = _list_get_next(p_node);
+    }
+
+    memcpy(p_term, &p_node->item, sizeof(TermType));
 
     return (TRUE);
 }
